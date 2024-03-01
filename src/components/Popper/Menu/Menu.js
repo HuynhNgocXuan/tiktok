aa4ff19ef1b9ab1f9fdfc7ddb452317c16a6c708
Huỳnh Ngọc Xuân <xuanhuynh.170204@gmail.com>
2024-03-01 14:36:00 +0700

    Search close bị che khi text dài -> OK "Anh Sơn, button của anh có một cái bug là khi dùng primary cùng to= hay href= thì nó ăn cái color = text color trong Global Style, anh có phát hiện hay chưa ạ?" - NDQ IT VN -> OK "Phần xử lý cuộn anh copy css thừa nha" - NDQ IT VN -> OK Review lại các components -> OK

---
+++ src/components/Popper/Menu/Menu.js
@@ -0,0 +1,75 @@
+import classNames from 'classnames/bind';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import styles from './Menu.module.scss';
+import MenuItems from './MenuItems';
+import Tippy from '@tippyjs/react/headless';
+import Header from './Header';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const cx = classNames.bind(styles);
+const defaultFn = () => {};
+
+function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn }) {
+    const [history, setHistory] = useState([{ data: items }]);
+    const current = history[history.length - 1];
+
+    const renderItems = () => {
+        return current.data.map((item, index) => {
+            const isParent = !!item.children;
+            return (
+                <MenuItems
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory((prev) => [...prev, item.children]);
+                        } else {
+                            onChange(item);
+                        }
+                    }}
+                />
+            );
+        });
+    };
+
+    const renderResult = (attrs) => (
+        <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+            <PopperWrapper className={cx('menu-popper')}>
+                {history.length > 1 && <Header title={current.title} onBack={handleBack} />}
+                <div className={cx('menu-body')}> {renderItems()}</div>
+            </PopperWrapper>
+        </div>  
+    );
+
+    const handleMenuToFirst = () => {
+        setHistory((prev) => prev.slice(0, 1));
+    };
+
+    const handleBack = () => {
+        setHistory((prev) => prev.slice(0, prev.length - 1));
+    };
+
+    return (
+        <Tippy
+            delay={[0, 300]}
+            interactive
+            hideOnClick={hideOnClick}
+            offset={[12, 8]}
+            placement="bottom-end"
+            render={renderResult}
+            onHide={handleMenuToFirst}
+        >
+            {children}
+        </Tippy>
+    );
+}
+
+Menu.propTypes = {
+    children: PropTypes.node.isRequired,
+    items: PropTypes.array,
+    hideOnClick: PropTypes.bool,
+    onChange: PropTypes.func,
+};
+
+export default Menu;
